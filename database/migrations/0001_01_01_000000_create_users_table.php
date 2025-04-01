ff6eeef853a736bee77cd0b9ec495fcc48ec0b70
rayan4-dot <rayanpro2023@gmail.com>
2025-04-01 14:32:33 +0000

    add profile picture

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('username')->unique()->nullable(); 
+            $table->string('profile_picture')->nullable();
             $table->string('email')->unique();
             $table->string('phone')->nullable();
 
